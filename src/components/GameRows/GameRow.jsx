54d770b8c9f45286752d25273a0d13481bd36381
Lucas Fraggioli <fraggioli.lucas@gmail.com>
2024-06-18 19:09:45 -0300

    preparando archivos

---
+++ src/components/GameRows/GameRow.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const GameRow = () => {
+  return <div>GameRow</div>;
+};
+
+export default GameRow;
---
+++ src/components/GameRows/Words.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import CONNECTION_GAMES from "../../lib/data";
+const Words = () => {
+  return <div>Words</div>;
+};
+
+export default Words;
